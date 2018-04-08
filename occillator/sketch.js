73e1716f88db4a3df4650b90e803e278940a63fc
GiladanoN <giladanon@gmail.com>
2018-04-08 18:25:20 +0300

    Ohhhhhhh funky~

--- occillator/sketch.js
+++ occillator/sketch.js
@@ -18,8 +18,10 @@ function setup()
   createCanvas(600,400);
   background(57);
 
-  colorMode(HSB, 255, 100, 100);
-  step = PI/64;
+  // noStrokse();
+  colorMode(HSB, 255, 100, 100, 10);
+  // step = PI/64;
+  step = PI/128;
 }
 
 function draw()
@@ -28,29 +30,44 @@ function draw()
   rectMode(CENTER);
   translate(width/2, height/2);
 
-  let angle;
   translate(-totalW/2, 0);
-  // let c = map(sin(curOff/8), -1, 1, 0, 255);
+
+  // c = (map(i, 0, divC, 0, 255) + (i+curOff)*10) %255;
+  // c = (curOff*10) % 255;
   // fill(c,80,100);
+
+  stripOff = curOff;
+  for (j=0; j<divC; j++, stripOff+=step) {
+    drawStrip(stripOff);
+  }
+
+  drawStrip(curOff+HALF_PI);
+  curOff += step;
+
+}
+
+function drawStrip(curOff)
+{
+  let angle;
   for (i=0; i<divC; i++) {
     push();
     translate(divW * (i+0.5), 0);
 
+    c = (curOff*10) % 255;
+    fill(c,80,100,5);
+
     let d = dist(i+0.5, 0, divC/2, 0);
     let angle = map(d, 0, divC, 0, PI) + curOff;
     // angle = curOff + i*step;
-
-    c = (map(i, 0, divC, 0, 255) + (i+curOff)*10) %255;
-    fill(c,80,100);
 
     let val = abs(sin(angle));
     let h = map(val, 0, 1, minH, maxH);
     rect(0,0,divW-2,h);
     pop();
   }
-  curOff += step;
-
 }
+
+
 
 let looping = true;
 function mouseClicked() {
