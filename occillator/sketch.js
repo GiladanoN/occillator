3453806257618399c3f4b3b8d02e6e438dd9c44a
GiladanoN <giladanon@gmail.com>
2018-04-08 20:07:20 +0300

    almost got it, still needs camera-work

--- occillator/sketch.js
+++ occillator/sketch.js
@@ -1,7 +1,7 @@
 
-let minH = 60;
-let maxH = 300;
-let divC = 20;
+let minH = 120;
+let maxH = 450;
+let divC = 15;
 
 let userDivs = (GetURLParameter('divs'));
 if (userDivs && userDivs >= 5)
@@ -13,6 +13,8 @@ let divW = totalW / divC;
 let step;
 let curOff = 0;
 
+xm=0;
+
 function setup()
 {
   createCanvas(600,400,WEBGL);
@@ -21,52 +23,78 @@ function setup()
   // noStroke();
   colorMode(HSB, 255, 100, 100, 10);
   // step = PI/64;
-  step = PI/128;
+  // step = PI/128;
+  step = PI/256;
 }
 
 function draw()
 {
+  xm+=1;
   background(57);
   rectMode(CENTER);
   // translate(width/2, height/2);
 
-  translate(-350,0,0);
-  rotateY(-QUARTER_PI);
-  translate(0,200,0);
-  rotateX(-PI/8);
-  translate(-totalW/2, 0, -500);
+  // translate(-350,0,0);
+  // translate(0,200,0);
+  rotateX(-PI);
+  rotateX(-QUARTER_PI/4);
+  // rotateZ(PI/8);
+  rotateY(4.75*QUARTER_PI);
+  translate(-totalW/2-500, -200, -500);
+  // translate(-totalW/2, 0, 0);
+
+  // camera(-230, 0, 100,
+  //        0,0,0,
+  //        1,2,0);
+  ortho(totalW/2, 1000, -500, 200, -300, 1500);
+  // ortho(-width / 2, width / 2, height / 2);
+  // rotateX(-QUARTER_PI+xm*0.01);
+  // rotateZ(QUARTER_PI+xm*0.01);
 
   // c = (map(i, 0, divC, 0, 255) + (i+curOff)*10) %255;
   // c = (curOff*10) % 255;
   // fill(c,80,100);
 
+  orbitControl();
+
   stripOff = curOff;
   stripStep = HALF_PI / divC;
-  for (j=0; j<divC; j++) {
-    // let d = dist(j+0.5, 0, divC/2, 0);
-    // let angle = map(d, 0, divC, 0, PI) + curOff;
-    translate(0,0,divW);
-    drawStrip(stripOff);
-    stripOff+=stripStep;
-  }
+  // for (j=0; j<divC; j++) {
+  //   // let d = dist(j+0.5, 0, divC/2, 0);
+  //   // let angle = map(d, 0, divC, 0, PI) + curOff;
+  //   translate(0,0,divW);
+  //   drawStrip(stripOff);
+  //   stripOff+=stripStep;
+  // }
 
   // drawStrip(curOff+HALF_PI);
+  drawStrip(curOff);
   curOff += step;
 
 }
 
 function drawStrip(curOff)
 {
+
+  stripOff = curOff;
+  stripStep = HALF_PI / divC;
+  for (j=0; j<divC; j++) {
+    let jd = dist(j+1.5, 0, divC/2, 0);
+    let jangle = map(jd, 0, divC, 0, PI) + curOff;
+    translate(0,0,divW);
+    // drawStrip(stripOff);
+
   let angle;
   for (i=0; i<divC; i++) {
     push();
     translate(divW * (i+0.5), 0);
 
     c = (curOff*10) % 255;
+    if(i==0) { c += 128; c %= 255; }
     fill(c,80,100,5);
 
     let d = dist(i+0.5, 0, divC/2, 0);
-    let angle = map(d, 0, divC, 0, PI) + curOff;
+    let angle = map(d, 0, divC, 0, PI) + curOff + jangle;
     // angle = curOff + i*step;
 
     let val = abs(sin(angle));
@@ -74,6 +102,10 @@ function drawStrip(curOff)
     box(divW-2, h, divW-2);
     pop();
   }
+
+    stripOff+=stripStep;
+  }
+
 }
 
 
